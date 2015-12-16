3dbd95a940f66a7994aa07c4b9974da854ce8af9
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2015-12-16 14:14:59 +0600

    Середина 7-го урока, продолжение следует

---
+++ snake/snake/HorizontalLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+
+        public HorizontalLine()
+        {
+            pList = new List<Point>;
+            Point p1 = new Point(4,4,)
+        }
+    }
+}
