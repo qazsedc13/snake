1871f0d480efc302d2b72774ba2aa4210a10c8f8
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2015-12-16 21:28:50 +0600

    Сделали змейку управляемой. Применили принцып инкапсуляции. Код лучше читается

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -19,36 +19,34 @@ namespace snake
             VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
             VerticalLine rightline = new VerticalLine(0, 24, 78, '+');
 
-            upline.Drow();
-            downline.Drow();
-            leftline.Drow();
-            rightline.Drow();
+            upline.Draw();
+            downline.Draw();
+            leftline.Draw();
+            rightline.Draw();
 
             // отрисовка точек
                         
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Drow();
+            snake.Draw();
 
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
+            while(true)
+            {
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
 
-
-
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
 
             Console.ReadLine();
 
+
         }
+
                
     }
 }
